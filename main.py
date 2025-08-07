13acfeca2d71eafdae4f9d44dac53b0089eecd89
PauloVGHA <paulovgha@gmail.com>
2025-08-07 16:24:51 +0300

    CH2 L3 assignment

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        clock.tick(60)
+        dt += clock.tick(60) / 1000.0  # Convert milliseconds to seconds
 
 if __name__ == "__main__":
     main()
