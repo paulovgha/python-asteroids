f83881dbd2adca4a26345edf0e7e817f80a35cb6
PauloVGHA <paulovgha@gmail.com>
2025-08-07 16:44:49 +0300

    CH3 L5 assignment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -10,8 +11,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    # Create a player instance
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         screen.fill("black", rect=None, special_flags=0)
+
+        player.draw(screen)
+
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
